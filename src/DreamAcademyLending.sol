42d7ca3fc9ea0eb707357c05c1c15286f2c0b3d8
koor00t <minjae.koo@dream-academy.io>
2023-03-21 15:23:16 +0900

    setting

--- src/DreamAcademyLending.sol
+++ src/DreamAcademyLending.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.13;
 
 import "./interfaces/IDreamAcademyLending.sol";
+import "./interfaces/IDreamOracle.sol";
 
-contract DreamAcademyLending is IDreamAcademyLending {
+contract DreamAcademyLending is IDreamAcademyLending, IDreamOracle {
 
     function deposit(address tokenAddress, uint256 amount) external payable {
 
